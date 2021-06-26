b053a40a3afc8417b91c6a69696cfdbd36d98ace
Renan Willian <renanwrs@gmail.com>
2021-06-26 12:49:23 -0300

    Fix login inside room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -19,7 +19,7 @@ type RoomParams = {
 }
 
 export function Room() {
-  const { user } = useAuth();
+  const { user, signInWithGoogle } = useAuth();
   const history = useHistory();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState('');
@@ -102,7 +102,7 @@ export function Room() {
                 <span>{user.name}</span>
               </div>
             ) : (
-              <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+              <span>Para enviar uma pergunta, <button onClick={signInWithGoogle}>faça seu login</button>.</span>
             ) }
             <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
